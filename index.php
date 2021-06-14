a39cf3cfc2969e4a71397fa133ce4d791c5bee44
jungmini <mins0428@gmail.com>
2021-06-14 14:15:05 +0900

    Update index.php

--- index.php
+++ index.php
@@ -3,4 +3,4 @@ echo '<div>Chrome Version : 91.xx </div>';
 echo '<br><br><div>Chrome 브라우저 설정 > Chrome 정보 에서 버전확인 위 버전과 같을경우 다음을 실행해주세요.</div>';
 echo '<div>버전이 틀릴경우 변경 요청주세요~</div>';
 
-echo '<a href="../">Chrome 변경 클릭</a>';
+echo '<a href="./WinGitSe/">Chrome 변경 클릭</a>';
